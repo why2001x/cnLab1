08c361b8d0519b308ec8c103a4182e9833da6c89
why2001 <dnd2001@qq.com>
2020-05-26 16:18:36 +0800

    Update datalink.c

--- datalink.c
+++ datalink.c
@@ -30,11 +30,6 @@
 
 #define MAX_SEQ 63 //帧的序号空间，应当是2^n-1
 #define NR_BUFS 32 //窗口大小，NR_BUFS=(MAX_SEQ+1)/2
-#define inc(k)       \
-	if (k < MAX_SEQ) \
-		k++;         \
-	else             \
-		k = 0 //计算k+1
 
 struct FRAME
 {								 //帧的内容
@@ -54,6 +49,11 @@ static int phl_ready = 0;				//物理层是否ready
 bool arrived[NR_BUFS];					//接收方输入缓存的bit map
 bool no_nak = true;						//是否发送了NAK，true则不再重复发送
 
+static unsigned char inc(unsigned char x)
+{
+	return ++x & MAX_SEQ;
+}
+
 static bool between(int a, int b, int c)
 { //如果b在a和c组成的窗口之间，则返回true，否则返回false
 	return ((a <= b) && (b < c)) || ((c < a) && (a <= b)) || ((b < c) && (c < a));
@@ -64,7 +64,7 @@ static void put_frame(unsigned char *frame, int len)
 	//将frame做CRC校验后附校验码一起发送到物理层
 	*(unsigned int *)(frame + len) = crc32(frame, len);
 	send_frame(frame, len + 4);
-	phl_ready = 0; //将数据发送到物理层后将物理层置为忙碌状态
+	phl_ready = false; //将数据发送到物理层后将物理层置为忙碌状态
 }
 
 static void send_data_frame(unsigned char frame_nr)
@@ -74,7 +74,7 @@ static void send_data_frame(unsigned char frame_nr)
 
 	s.kind = FRAME_DATA;
 	s.seq = frame_nr;
-	s.ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1);
+	s.ack = (frame_expected + MAX_SEQ) & MAX_SEQ;
 	memcpy(s.data, out_buffer[frame_nr % NR_BUFS], PKT_LEN); //将out_buffer里的缓存复制到帧frame的data域中
 
 	dbg_frame("Send DATA %d %d, ID %d\n", s.seq, s.ack, *(short *)s.data);
@@ -91,7 +91,7 @@ static void send_ack_frame(void)
 	struct FRAME s;
 
 	s.kind = FRAME_ACK;
-	s.ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1);
+	s.ack = (frame_expected + MAX_SEQ) & MAX_SEQ;
 
 	dbg_frame("Send ACK  %d\n", s.ack);
 
@@ -105,7 +105,7 @@ static void send_nak_frame(void)
 	struct FRAME s;
 
 	s.kind = FRAME_NAK;
-	s.ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1);
+	s.ack = (frame_expected + MAX_SEQ) & MAX_SEQ;
 
 	no_nak = false;
 
@@ -115,25 +115,22 @@ static void send_nak_frame(void)
 	stop_ack_timer();
 }
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	//初始化
 	nbuffered = 0; //初始没有输出帧被缓存
-	int i;
-	for (i = 0; i < NR_BUFS; i++) //初始时没有输入帧被缓存
+	for (int i = 0; i < NR_BUFS; i++) //初始时没有输入帧被缓存
+	{
 		arrived[i] = false;
+	}
 	int event, oldest_frame;
-	struct FRAME f;
 	int len = 0; //收到data的长度
 
 	protocol_init(argc, argv); //协议初始化
-	lprintf("Designed by Hong Zhilong, based on Teacher Jiang's code.\nBuild: "__DATE__
-		"  "__TIME__
-		"\n");
 
 	disable_network_layer();
 
-	for (;;)
+	for (struct FRAME f;;)
 	{
 		event = wait_for_event(&oldest_frame);
 
@@ -147,7 +144,7 @@ int main(int argc, char **argv)
 			break;
 
 		case PHYSICAL_LAYER_READY: //当物理层ready时，将phy_ready置为1以便之后enable网络层
-			phl_ready = 1;
+			phl_ready = true;
 			break;
 
 		case FRAME_RECEIVED:								 //当物理层收到一帧时
@@ -175,7 +172,7 @@ int main(int argc, char **argv)
 					memcpy(in_buffer[f.seq % NR_BUFS], f.data, PKT_LEN); //将接收到的帧的data域复制至输入缓存中
 					while (arrived[frame_expected % NR_BUFS])			 //当收到接收方窗口下界的一帧时，对这一帧以及之后收到的帧进行处理
 					{
-						put_packet(in_buffer[frame_expected % NR_BUFS], len - 7); //将输入缓存送至网络层
+						put_packet(in_buffer[frame_expected % NR_BUFS], PKT_LEN); //将输入缓存送至网络层
 						no_nak = true;
 						arrived[frame_expected % NR_BUFS] = false;
 						inc(frame_expected); //将窗口前移一位
@@ -185,10 +182,10 @@ int main(int argc, char **argv)
 				}
 			}
 
-			if ((f.kind == FRAME_NAK) && between(ack_expected, (f.ack + 1) % (MAX_SEQ + 1), next_frame_to_send))
+			if ((f.kind == FRAME_NAK) && between(ack_expected, (f.ack + 1) & MAX_SEQ, next_frame_to_send))
 				//如果收到的是nak帧且ack的下一帧在发送方窗口里，则发送ack的下一帧
 			{
-				send_data_frame((f.ack + 1) % (MAX_SEQ + 1));
+				send_data_frame((f.ack + 1) & MAX_SEQ);
 				dbg_frame("Recv NAK with ACK %d\n", f.ack);
 			}
 
